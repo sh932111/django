e3fae262ce2862cce100447c2e2af4ca6470b3d5
Yoga <sh932111@gmail.com>
2016-07-27 11:02:04 +0800

    build blog detail

--- website/settings.py
+++ website/settings.py
@@ -119,4 +119,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ( os.path.join('static'), )
